e544a3e5cabdb8d7f971fa737a069b39d6c3c1ca
yigh <yizhou12345@gmail.com>
2013-11-28 04:28:07 -0600

    at least the shine shows up

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,21 +2,45 @@ var Game = {};
 Game.Construct = function() 
 {
     Game.initialized = 0;
-    Game.T=0;
-    Game.drawT=0;
-    Game.fps=30;
-    Game.defaultBg='background_img.jpg';
-    gLoadAssets();
+    Game.T = 0;
+    Game.drawT = 0;
+    Game.fps = 30;
+    Game.defaultBg = 'background_img.jpg';
+    Game.targetX=0;
+    Game.targetY=0;
     /* Game Variables */
     Game.currency = 0;
-    Game.time=new Date().getTime();
+    Game.time = new Date().getTime();
+    Game.lastClick = 0;
+    Game.autoclickerDetected = 0;
+    Game.BigCookieState = 0;
+    Game.BigCookieSize = 0;
+    Game.mouseEarnRate = 1;
+    Game.clicks = 0;
+    Game.recalculateEarnRate = 1;
+
+    gLoadAssets();
+    /* Set listeners for the click target */
+    gInitClickTarget();
     gMain();
-    function gAddCurrency (amt) {Game.currency += amt;};
+
+    function gInitClickTarget() {
+        var bigCookie = get('bigCookie');
+        AddEvent(bigCookie,'click',gHandleClick);
+        AddEvent(bigCookie,'mousedown',function(event){Game.BigCookieState=1;});
+        AddEvent(bigCookie,'mouseup',function(event){Game.BigCookieState=2;});
+        AddEvent(bigCookie,'mouseout',function(event){Game.BigCookieState=0;});
+        AddEvent(bigCookie,'mouseover',function(event){Game.BigCookieState=2;});
+        AddEvent(document,'mousemove', gGetMouseCoords);
+    }
+    function gEarn (amt) {Game.currency += amt;};
     function gLoadAssets () {
         Game.assets = {};
         var pics=[
                   'blackGradient.png',
                   'shadedBorders.png',
+                  'target.png',
+                  'shine.png',
                   'background_img.jpg'
                   ];
         for(var i in pics) {
@@ -26,6 +50,34 @@ Game.Construct = function()
             Game.assets[pics[i]] = img;
         }
     }
+    function gGetMouseCoords(e)
+    {
+        var posx=0;
+        var posy=0;
+        if (!e) var e=window.event;
+        if (e.pageX||e.pageY)
+        {
+            posx=e.pageX;
+            posy=e.pageY;
+        }
+        else if (e.clientX || e.clientY)
+        {
+            posx=e.clientX+document.body.scrollLeft+document.documentElement.scrollLeft;
+            posy=e.clientY+document.body.scrollTop+document.documentElement.scrollTop;
+        }
+        var el=get('sectionLeft');
+        var x=0;
+        var y=0;
+        while(el && !isNaN(el.offsetLeft) && !isNaN(el.offsetTop))
+        {
+            x+=el.offsetLeft-el.scrollLeft;
+            y+=el.offsetTop-el.scrollTop;
+            el=el.offsetParent;
+        }
+        Game.mouseX=posx-x;//Math.min(Game.w,Math.max(0,posx-x));
+        Game.mouseY=posy-y;//Math.min(Game.h,Math.max(0,posy-y));
+    }
+
     function gDrawBackground() {
         if (!Game.Background)
         {
@@ -52,14 +104,52 @@ Game.Construct = function()
 
     }
 
-    function gDraw() {
-        gDrawBackground();
+    function gDrawCookie() {
+        Game.LeftBackground.clearRect(0,0,Game.LeftBackground.canvas.width,Game.LeftBackground.canvas.height);
+        Game.targetX=Math.floor(Game.LeftBackground.canvas.width/2);
+        Game.targetY=Math.floor(Game.LeftBackground.canvas.height*0.4);
+        var s=512;
+        var x=Game.targetX-s/2;
+        var y=Game.targetY-s/2;
+        Game.LeftBackground.globalAlpha=0.5;
+        Game.LeftBackground.drawImage(Game.assets['shine.png'],x,y,s,s);
+        Game.LeftBackground.globalAlpha=1;
+        var s=256*Game.BigCookieSize;
+        var x=Game.targetX-s/2;
+        var y=Game.targetY-s/2;
+        Game.LeftBackground.drawImage(Game.assets['target.png'],x,y,s,s);
+    }
 
+    function gDraw() 
+    {
+        gDrawBackground();
+        gDrawCookie();
         get("currency").innerHTML="You have no monies";
         Game.drawT++;
     }
-    function gMain() {
+    function gMain() 
+    {
         gDraw();
         setTimeout(gMain,1000/Game.fps);
+    }
+    function gHandleClick() 
+    {
+        if (new Date().getTime()-Game.lastClick<1000/250)
+        {} 
+        else {
+            gEarn(Game.mouseEarnRate);
+            Game.handmadeCurrency+=gComputeCurrencyGain();
+            /* TODO : import functionality for Game.CookieNumbers add */
+            Game.clicks++;
+        }
+        Game.mouseGain += Game.mouseEarnRate;
+        Game.lastClick=new Date().getTime();
+    }
+
+    /* TODO : Modify later for new buildings and achievements... etc */
+    function gComputeCurrencyGain() {
+        var base = 0, mul = 1, raw;
+        /* Calculate base and mul based on purchased stuff */
+        Game.recalculateEarnRate = 0;
     }
 };
--- assets/js/lib.js
+++ assets/js/lib.js
@@ -22,3 +22,32 @@ CanvasRenderingContext2D.prototype.fillPattern=function(img,X,Y,W,H,iW,iH)
             this.drawImage(img,X+x,Y+y,iW,iH);
 }
 
+/* Taken from Orteil's code */
+function Beautify(what,floats)//turns 9999999 into 9,999,999
+{
+    var str='';
+    if (!isFinite(what)) return 'Infinity';
+    if (what.toString().indexOf('e')!=-1) return what.toString();
+    what=Math.round(what*10000000)/10000000;//get rid of weird rounding errors
+    if (floats>0)
+    {
+        var floater=what-Math.floor(what);
+        floater=Math.round(floater*10000000)/10000000;//get rid of weird rounding errors
+        var floatPresent=floater?1:0;
+        floater=(floater.toString()+'0000000').slice(2,2+floats);//yes this is hacky (but it works)
+        if (parseInt(floater)===0) floatPresent=0;
+        str=Beautify(Math.floor(what))+(floatPresent?('.'+floater):'');
+    }
+    else
+    {
+        what=Math.floor(what);
+        what=(what+'').split('').reverse();
+        for (var i in what)
+        {
+            if (i%3==0 && i>0) str=','+str;
+            str=what[i]+str;
+        }
+    }
+    return str;
+}
+

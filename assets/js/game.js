590351acbf3e366c34823dc867489a31d0a7e265
yigh <yizhou12345@gmail.com>
2013-11-28 01:39:54 -0600

    Buggy drawing functionality

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,50 @@
 var Game = {};
-
 Game.Construct = function() 
 {
     Game.initialized = 0;
-    
+    Game.background=get('backgroundCanvas').getContext('2d');
+    Game.background.canvas.width=Game.background.canvas.parentNode.offsetWidth;
+    Game.background.canvas.height=Game.background.canvas.parentNode.offsetHeight;
+    Game.leftBackground=get('backgroundLeftCanvas').getContext('2d');
+    Game.leftBackground.canvas.width=Game.leftBackground.canvas.parentNode.offsetWidth;
+    Game.leftBackground.canvas.height=Game.leftBackground.canvas.parentNode.offsetHeight;
+    window.addEventListener('resize', function(event)
+    {
+        Game.background.canvas.width=Game.background.canvas.parentNode.offsetWidth;
+        Game.background.canvas.height=Game.background.canvas.parentNode.offsetHeight;
+        Game.leftBackground.canvas.width=Game.leftBackground.canvas.parentNode.offsetWidth;
+        Game.leftBackground.canvas.height=Game.leftBackground.canvas.parentNode.offsetHeight;
+    });
+ 
+    Game.LoadAssets = function() 
+    {
+      Game.assets = {};
+      var pics=[
+                'blackGradient.png',
+                'background_img.jpg',
+                'download.jpeg'
+                ];
+        for(var i in pics) {
+            var img = new Image();
+            img.src = "assets/img/" + pics[i];
+            Game.assets[pics[i]] = img;
+        }
+    };
+    Game.DrawBackground = function() 
+    {
+        img = Game.assets['download.jpeg'];
+        if(img != null) {
+            console.log("Drawing background");
+            var pattern = Game.background.createPattern(img, "repeat");
+            Game.background.rect(0, 0, Game.background.canvas.width, Game.background.canvas.height);
+            Game.background.fillStyle = pattern;
+            Game.background.fill();
+        } else {
+            console.log("Error: Can't find background_img.jpg resource");
+        }
+    };
+
+    Game.LoadAssets();
+    Game.assets['background_img.jpg'].onload = Game.DrawBackground();
+    Game.time=new Date().getTime();
 };
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
 window.onload=function()
 {
+    Game.Construct();
+    get("no-js").innerHTML='<div class="title">Loading</div>';
 };
